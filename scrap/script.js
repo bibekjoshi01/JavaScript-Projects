9161a761eb3a6851893eb387d92d412f571b6962
bibekjoshi01 <bibekjoshi34@gmail.com>
2023-04-15 01:17:14 +0545

    blog created

---
+++ scrap/script.js
@@ -0,0 +1,10 @@
+const table = document.querySelector(".mGrid");
+
+dataHeadings = table.querySelectorAll('th')
+
+console.log(dataHeadings)
+
+for (let i=0; i<dataHeadings.length; i++){
+    console.log(dataHeadings[i])
+}
+
